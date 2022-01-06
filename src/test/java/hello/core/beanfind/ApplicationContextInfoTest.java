1bd013e020cee59de0706e7a44473b1be99f2257
leesh125 <chelsea12345@naver.com>
2022-01-06 17:34:47 +0900

    컨테이너에 등록된 모든 빈 조회 - 테스트 케이스를 통해 빈 조회(.getBeanDefinition)

---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,43 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+//      ac.getBeanDefinitionNames() : 스프링에 등록된 모든 빈 이름을 조회한다.
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+//          ac.getBean() : 빈 이름으로 빈 객체(인스턴스)를 조회한다.
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+
+
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            // ac.getBeanDefinition(빈 이름) : 해당 빈에 대한 메타데이터 정보를 갖고옴
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //  ROLE_APPLICATION : 일반적으로 사용자가 정의한 빈
+            //  ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+}
