fef15b8edb5eaf43451a608494b81f27fb931b3e
leesh125 <chelsea12345@naver.com>
2022-01-08 03:47:15 +0900

    @Configuration과 싱글톤 - 사용자가 memberService와 memberRepo를 사용하면 스프링은 AppConfig에 있는 bean을 호출함과 동시에 memberRepo는 두번 호출이 되는걸로 생각(하지만 스프링은 싱글톤을 보장)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,39 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService() -> new MemoryMemberRepository()
+    // @Bean orderService() -> new MemoryMemberRepository()
+
+    /*
+        개발자의 가시적인 호출 순서
+          1. call AppConfig.memberService
+          2. call AppConfig.memberRepository
+          3. call AppConfig.memberRepository
+          4. call AppConfig.orderService
+          5. call AppConfig.memberRepository
+
+        스프링의 호출 순서(싱글톤 보장)
+          1. call AppConfig.memberService
+          2. call AppConfig.memberRepository
+          3. call AppConfig.orderService
+     */
+
+
     @Bean // 스프링 컨테이너에 등록이 됨
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -17,4 +17,9 @@ public class MemberServiceImpl implements MemberService{
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
+    // 테스트 용도(config의 두 번 호출? 의 테스트)
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
