119851f3387fc0cb70e48f3b513d244d0408ae0c
leesh125 <chelsea12345@naver.com>
2022-01-11 01:51:13 +0900

    필터 - 사용자 annotation 생성 가능, ComponentScan에서 filter를 사용해 빈으로 등록할 애노테이션, 제외할 애노테이션 설정 가능

---
+++ src/test/java/hello/core/scan/Filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,37 @@
+package hello.core.scan.Filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.context.annotation.ComponentScan.*;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext();
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        assertThat(beanA).isNotNull();
+
+        assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class));// 오류 발생(excludeComponent로 했기에 제외된다)
+    }
+
+    @Configuration
+    @ComponentScan(                // type: annotaion과 관련된 필터(기본값 = .ANNOTATION)
+            includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class) // BeanB는 스프링 빈으로 등록하지 않겠다.
+    ) // 나만의 component를 scan 할 수 있는 기능 생성
+    static class ComponentFilterAppConfig {
+
+    }
+}
---
+++ src/test/java/hello/core/scan/Filter/MyExcludeComponent.java
@@ -0,0 +1,11 @@
+package hello.core.scan.Filter;
+
+import java.lang.annotation.*;
+
+// ComponentScan에 추가하기 위한 코드들(아래 3줄)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+    
+}
