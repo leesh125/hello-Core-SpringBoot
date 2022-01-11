b77e38aee53ffdfe66313d5f7573ef887bda2c1f
leesh125 <chelsea12345@naver.com>
2022-01-11 19:24:06 +0900

    @Autowired 필드 명, @Qualifier, @Primary - 같은 인터페이스의 두 개의 구현체가 둘 다 스프링 빈으로 등록될 경우, 해결방법들

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,9 +2,12 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component // 두 개의 스프링 빈 등록(rateDisCount도 등록했음)
+@Primary // 같은 인터페이스에서 두 개 이상 빈이 등록 될 경우 우선권을 가짐
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000; // 1000원 할인
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,10 +6,11 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final이 붙은(필수 값) 필드를 가지고 생성자 주입 코드를 똑같이 만들어줌
+// @RequiredArgsConstructor // final이 붙은(필수 값) 필드를 가지고 생성자 주입 코드를 똑같이 만들어줌
 public class OrderServiceImpl implements OrderService{
 
     // final을 통해 생성자에서 값이 설정되지 않을 경우 오류 발생
@@ -25,11 +26,32 @@ public class OrderServiceImpl implements OrderService{
      */
 
     // 1. 생성자 주입 - lombok에 의해 자동 생성
-//    @Autowired // 최근에는 생성자를 딱 1개 두고, @Autowired를 생략하는 방법을 사용
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
+    @Autowired // 최근에는 생성자를 딱 1개 두고, @Autowired를 생략하는 방법을 사용
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;               // qualifire로 등록한 것 중 원하는 것 이름 넣어주면 그것이 생성자로 들어감
+        this.discountPolicy = discountPolicy;
+        // discountPolicy의 하위 구현체 2개가 동시에 스프링 빈으로 등록된 경우
+        // 필드명 또는 파라미터 이름으로 빈 이름을 매칭(rateDiscountPolicy)
+    }
+
+
+    /* 2. 빈 두개 이상 등록 해결(@Qualifier로 따로 이름 지정)
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;               // qualifire로 등록한 것 중 원하는 것 이름 넣어주면 그것이 생성자로 들어감
 //        this.discountPolicy = discountPolicy;
+//        // discountPolicy의 하위 구현체 2개가 동시에 스프링 빈으로 등록된 경우
+//        // 필드명 또는 파라미터 이름으로 빈 이름을 매칭(rateDiscountPolicy)
 //    }
+
+    /* 1. 빈 두개 이상 등록 해결(필드명, 파라미터로 매칭)
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+        // discountPolicy의 하위 구현체 2개가 동시에 스프링 빈으로 등록된 경우
+        // 필드명 또는 파라미터 이름으로 빈 이름을 매칭(rateDiscountPolicy)
+    }*/
 
     /* 2. 수정자 주입(setter 주입)  // 외부에서 수정이 가능하기에 비추
     @Autowired
