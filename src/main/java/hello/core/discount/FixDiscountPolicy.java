a3d400b55cc3351eac785bba1a832b1494e70155
leesh125 <chelsea12345@naver.com>
2022-01-11 19:04:43 +0900

    조회 빈이 2개 이상 - 문제 - 스프링 컨테이너에서 생성한 빈을 토대로 의존관계를 주입하려하는데 두 개의 구체화된 DiscountPolicy가 둘 다 등록이 되었을 경우 오류

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component // 두 개의 스프링 빈 등록(rateDisCount도 등록했음)
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000; // 1000원 할인
