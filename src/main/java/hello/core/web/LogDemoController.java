fc0531370da244ba46c9f9680fe0c17c617708d6
leesh125 <chelsea12345@naver.com>
2022-01-12 18:15:38 +0900

    스코프와 Provider - ObjectProvider를 활용하여 사용자의 요청(request)가 오면 myLogger빈을 생성하도록 해결 (여러 요청이 와도 요청마다 다른 request scope으로 처리)

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,11 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider; // ObjectProvider를 통해 controller에서 고객 요청이 오면 빈을 생성
 
     @RequestMapping("log-demo")
     @ResponseBody // 화면이 없이 문자만 입력받기
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL); // 로그와 함께 url을 화면에 출력하기 위해
 
