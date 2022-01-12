36967c10245f3e11e9bedc063df3e1d7572c2630
leesh125 <chelsea12345@naver.com>
2022-01-12 17:22:03 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제해결(ObjectProvider방법, JSR-330 Provider방법{javax.inject})

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -37,20 +39,28 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton") // default이기에 안해도 됨
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입(프로토타입 이기에)
+        // private final PrototypeBean prototypeBean; // 생성 시점에 주입(프로토타입 이기에)
 
-        @Autowired
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+        // 항상 새로운 프로토타입 빈 생성 : ObjectProvider 방법
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider; // 지정한 빈을 컨테이너에서 대신 찾아줌
+
+        // 항상 새로운 프로토타입 빈 생성 : javax.inject 방법
+          @Autowired
+          private Provider<PrototypeBean> prototypeBeanProvider; // 지정한 빈을 컨테이너에서 대신 찾아줌
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean){
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); // 빈을 찾아서 새로운 빈 항상 생성
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
