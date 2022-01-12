b19b2d4284b0b27712c894ee1712a971d0984572
leesh125 <chelsea12345@naver.com>
2022-01-12 18:29:58 +0900

    스코프와 프록시 - myLogger 객체를 프록시 기술로 가짜 객체를 생성 -> 가짜 프록시 객체는 요청이 오면 그때 내부에서 진짜 빈을 요청(싱글톤 처럼 동작하지만 싱글톤이 아님)

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,13 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider; // ObjectProvider를 통해 controller에서 고객 요청이 오면 빈을 생성
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody // 화면이 없이 문자만 입력받기
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass()); // 가짜 프록시 객체는 요청이 오면 그때 내부에서 진짜 빈을 요청하는 위임 로직이 들어있다.
         myLogger.setRequestURL(requestURL); // 로그와 함께 url을 화면에 출력하기 위해
 
         myLogger.log("controller test");
